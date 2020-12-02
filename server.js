ab1b36af3323a0201045e17c6e5f21ee37846fbc
vaibhavi-git <54622107+vaibhavi-git@users.noreply.github.com>
2020-12-03 00:31:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Hello World From nodejs app</h1> \n');
+  res.send('<h1 style="color:green;">Hello World</h1> \n');
 });
 
 app.listen(PORT, HOST);
